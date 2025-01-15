4bec480ce018e50000fde25e1b70cd8c96bfdf05
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-15 17:28:58 -0500

    Create waterfoxnovnc.sh

---
+++ waterfoxnovnc.sh
@@ -0,0 +1,3 @@
+cd ~/
+wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xf waterfox-6.5.3.tar.bz2
+git clone https://github.com/gitxpresso/linux-novnc.git
