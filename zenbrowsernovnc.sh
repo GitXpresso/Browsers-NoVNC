d934716536e724c708856a1321fe53b8ca9d0e5f
GitXpresso <gitxpresso@outlook.com>
2025-01-16 16:56:08 +0000

    fixed tar command to work properly and deleted zen tar bz2 file

--- zenbrowsernovnc.sh
+++ zenbrowsernovnc.sh
@@ -2,7 +2,7 @@
 echo "installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
-wget https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.bz2 && sudo tar -xzvf zen.linux-x86_64.tar.bz2 -C /usr/lib/
+wget https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.bz2 && sudo tar -xvf zen.linux-x86_64.tar.bz2 -C /usr/lib
 git clone https://github.com/gitxpresso/linux-novnc.git
 echo "creating zen.desktop using cat here document"
 cat << EOF > /usr/share/applications
