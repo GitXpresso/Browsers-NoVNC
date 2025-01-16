88a966feffce33065b7896fe0f812ac4b0bdfe1e
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-15 19:50:33 -0500

    Update waterfoxnovnc.sh
    Added "tar" to the required packages

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -1,6 +1,6 @@
 
 echo "Installing the required packages in order for the script to worl properly"
-sudo  apt install -y wget novnc websockify tigervnc-standalone-server 
+sudo  apt install -y wget novnc websockify tigervnc-standalone-server tar
 cd ~/
 wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xf waterfox-6.5.3.tar.bz2
 git clone https://github.com/gitxpresso/linux-novnc.git
