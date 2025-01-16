7ef9f8c1d9be75bfe82f2bf53b2b876a436e7ebd
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-15 20:05:06 -0500

    Update waterfoxnovnc.sh
    Made where novnc will not prompt user a password

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -6,6 +6,6 @@ sudo  apt install -y wget novnc websockify tigervnc-standalone-server tar
 cd ~/
 wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xf waterfox-6.5.3.tar.bz2
 git clone https://github.com/gitxpresso/linux-novnc.git
-tigervncserver -xstartup ~/waterfox/waterfox -geometry 800x600 -localhost no :0 
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup ~/waterfox/waterfox -geometry 800x600 -localhost no :0 
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 echo "Finished, Now go to https://localhost:5900 to access waterfox the vnc server"
