a31351c6d5f4660a7b22b67fa97d0eb6e7456d76
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-16 15:18:06 -0500

    Update waterfoxnovnc.sh

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -5,7 +5,13 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
 wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib
+sudo mv -f ~/waterfox.desktop /usr/share/applications
+git clone https://github.com/gitxpresso/linux-novnc.git
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -xstartup /usr/lib/waterfox/waterfox  -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+echo "Finished, Now go to https://localhost:5900 to access waterfox the vnc server"
 echo "creating waterfox.desktop using the cat here document"
+
 cat << EOF > ~/waterfox.desktop
 [Desktop Entry]
 Version=1.0
@@ -28,10 +34,7 @@ Exec=/usr/lib/waterfox/waterfox -new-window
 
 [Desktop Action new-private-window]
 Name=Open a New Private Window
-Exec=/usr/lib/waterfox/waterfox -private-window
+Exec=/usr/lib/waterfox/waterfox -private-windo
 EOF
-sudo mv -f ~/waterfox.desktop /usr/share/applications
-git clone https://github.com/gitxpresso/linux-novnc.git
-tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/lib/waterfox/waterfox -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
-websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-echo "Finished, Now go to https://localhost:5900 to access waterfox the vnc server"
+
+
