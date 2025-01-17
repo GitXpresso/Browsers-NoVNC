186957b0488b1b9d2a52ff3c406e595e7c5ece80
GitXpresso <gitxpresso@outlook.com>
2025-01-17 14:21:17 +0000

    waterfox now opens when vnc starts

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -7,34 +7,9 @@ cd ~/
 wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib
 sudo mv -f ~/waterfox.desktop /usr/share/applications
 git clone https://github.com/gitxpresso/linux-novnc.git
-tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -xstartup /usr/lib/waterfox/waterfox  -geometry 1366x768 -localhost no :0
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/lib/waterfox/waterfox -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+/usr/lib/waterfox/waterfox --display=:0
 echo "Finished, Now go to https://localhost:5900 to access waterfox the vnc server"
 echo "creating waterfox.desktop using the cat here document"
-
-cat << EOF > ~/waterfox.desktop
-[Desktop Entry]
-Version=1.0
-Name=Waterfox Web Browser
-Comment=Browse the World Wide Web
-GenericName=Web Browser
-Keywords=Internet;WWW;Browser;Web;Explorer
-Exec=/usr/lib/waterfox/waterfoxTerminal=false %u
-X-MultipleArgs=false
-Type=Application
-Icon=/usr/lib/waterfox/chrome/icons/default/default48.png
-Categories=GNOME;GTK;Network;WebBrowser;
-MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;
-StartupNotify=true
-Actions=new-window;new-private-window;
-
-[Desktop Action new-window]
-Name=Open a New Window
-Exec=/usr/lib/waterfox/waterfox -new-window
-
-[Desktop Action new-private-window]
-Name=Open a New Private Window
-Exec=/usr/lib/waterfox/waterfox -private-windo
-EOF
-
 
