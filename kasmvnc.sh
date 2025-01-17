95a2a28104404eab756480a7e440d7fefe2c20a0
GitXpresso <gitxpresso@outlook.com>
2025-01-17 14:19:59 +0000

    firefox opens when vnc starts

--- kasmvnc.sh
+++ kasmvnc.sh
@@ -1,9 +1,4 @@
 wget https://github.com/kasmtech/KasmVNC/releases/download/v1.3.3/kasmvncserver_bookworm_1.3.3_amd64.deb
 sudo apt update
-sudo apt install ./kasmvncserver_*.deb
+sudo aptitude install ./kasmvncserver_*.deb
 
-if sudo addgroup $USER ssl-cert ; then
-    echo -e "sudo addgroup $USER ssl-cert succeeded don't have to do usermod"
-else
-    sudo usermod -aG ssl-cert $usermod
-fi
