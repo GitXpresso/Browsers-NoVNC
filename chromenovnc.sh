356ea9cb73b3223b8126b519beb6d95f1c196b4d
GitXpresso <gitxpresso@outlook.com>
2025-01-17 15:00:00 +0000

    added -y

--- chromenovnc.sh
+++ chromenovnc.sh
@@ -5,4 +5,4 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
