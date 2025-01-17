d7d5ca8cdc61507cd3abfa3aaa47e6f23f53cc7c
GitXpresso <gitxpresso@outlook.com>
2025-01-17 20:34:38 +0000

    Made floorpnovnc.sh

---
+++ floorpnovnc.sh
@@ -0,0 +1,3 @@
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
+cd ~/
+wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/v11.22.0/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2
