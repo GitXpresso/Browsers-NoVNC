f7ba62007a32a57438bde99ebbfdba89b93e3802
GitXpresso <gitxpresso@outlook.com>
2025-01-17 20:57:40 +0000

    Renamed floorpnovnc to librewolfnovnc until i found a way to run floorp in kasmvnc dockerfile

--- librewolfnovnc.sh
+++ librewolfnovnc.sh
@@ -3,8 +3,10 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
-wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2 -C /usr/lib
+sudo apt update && sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf -y
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-/usr/lib/floorp/floorp --display=:0
-echo -e "floorp has started go to https://localhost:6080 to access your vnc session"
+librewolf --display=:0
+echo -e "librewolf has started go to https://localhost:6080 to access your vnc session"
