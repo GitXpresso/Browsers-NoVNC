7cb4db0dfc9bf53bc06bc86154163d01fa08bcaa
GitXpresso <gitxpresso@outlook.com>
2025-01-17 15:48:03 +0000

    h

--- operanovnc.sh
+++ operanovnc.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+echo "Updating Your System"
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
+cd ~/
 wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
 sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
 sudo apt update
