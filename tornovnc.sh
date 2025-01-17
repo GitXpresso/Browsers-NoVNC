91ede151b45a5db23253931413a990debe319210
GitXpresso <gitxpresso@outlook.com>
2025-01-17 16:46:37 +0000

    added /usr/lib/ and made file automatically delete tar.xz file

--- tornovnc.sh
+++ tornovnc.sh
@@ -4,7 +4,7 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
-wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && sudo tar -xf tor-browser-linux-x86_64-14.0.4.tar.xz 
+wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && sudo tar -xf tor-browser-linux-x86_64-14.0.4.tar.xz -C /usr/lib/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
 git clone https://github.com/gitxpresso/linux-novnc.git
 sudo mv -f usr/lib/tor-browser/start-tor-browser.desktop /usr/share/applications
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
