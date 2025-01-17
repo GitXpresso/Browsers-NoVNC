dda295cefc110df09cd1613b82bb7f65986dbab7
GitXpresso <gitxpresso@outlook.com>
2025-01-17 14:53:24 +0000

    added chromenovnc.sh

---
+++ chromenovnc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Updating Your System"
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
+cd ~/
