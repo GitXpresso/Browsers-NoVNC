ff648a4a6a3a3b9d47bbd2369048acfd1e5e332f
GitXpresso <gitxpresso@outlook.com>
2025-01-17 16:56:06 +0000

    finished chromenovnc.sh

--- chromenovnc.sh
+++ chromenovnc.sh
@@ -9,3 +9,4 @@ sudo apt install -y ./google-chrome-stable_current_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 google-chrome --display=:0
+echo -e "chrome has started go to https://localhost:6080 to access your vnc session"
