be3e2fa5c58f567d7bab514eb8ced352255d3896
GitXpresso <gitxpresso@outlook.com>
2025-01-18 01:10:50 +0000

    Finished seamonkeynovnc.sh

--- seamonkeynovnc.sh
+++ seamonkeynovnc.sh
@@ -3,7 +3,8 @@ echo "updating system to successfully install required packages"
 sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11
-printf 'deb [signed-by=/usr/share/keyrings/ubuntuzilla.gpg] https://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main\n' | sudo tee /etc/apt/sources.list.d/ubuntuzilla.list >/dev/null
-sudo gpg --no-default-keyring --keyring /usr/share/keyrings/ubuntuzilla.gpg --keyserver keyserver.ubuntu.com --recv-keys 2667CA5C
-sudo apt update
-sudo apt install seamonkey-mozilla-build
+wget https://archive.seamonkey-project.org/releases/2.53.20/linux-x86_64/en-US/seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 && sudo tar -xvf seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+/usr/lib/seamonkey/seamonkey --display=:0
+echo -e "librewolf has started go to https://localhost:6080 to access your vnc session"
