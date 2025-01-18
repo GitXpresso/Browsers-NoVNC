5114e747461193d9aa5512dd7b8ae663082b5e7a
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-18 10:58:50 -0500

    Update bravenovnc.sh
    Got brave to work

--- bravenovnc.sh
+++ bravenovnc.sh
@@ -6,5 +6,6 @@ sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 curl -fsS https://dl.brave.com/install.sh | sh
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
 brave-browser --display=:0
 echo -e "brave has started go to https://localhost:6080 to access your vnc session"
