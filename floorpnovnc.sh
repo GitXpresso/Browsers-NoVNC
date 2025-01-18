284e581ded5f3d53fe7252be68fbefd84ddb344f
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-18 16:51:54 -0500

    Update floorpnovnc.sh
    Floor works and script automatically removes tar file after tar file is finished extracting

--- floorpnovnc.sh
+++ floorpnovnc.sh
@@ -4,7 +4,7 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 dialog
 cd ~/
-wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2 -C /usr/lib
+wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf floorp-11.22.0.linux-x86_64.tar.bz2 
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
