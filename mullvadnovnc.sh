ae2e7ae8b492f558eab586cddec708af951aecde
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-18 09:37:50 -0500

    Create mullvadnovnc.sh

---
+++ mullvadnovnc.sh
@@ -0,0 +1,7 @@
+echo "Downloading the Mullvad signing key using curl"
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+echo "Adding the Mullvad repository server to apt"
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+echo -e "Installing the package(mullvad)"
+sudo apt update
+sudo apt install mullvad-browser
