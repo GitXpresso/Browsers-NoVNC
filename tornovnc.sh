978e729bce5c6bc3076b020fcbb4909102fed2b8
GitXpresso <gitxpresso@outlook.com>
2025-01-18 23:51:20 +0000

    Fixed tornovnc.sh

--- tornovnc.sh
+++ tornovnc.sh
@@ -4,9 +4,10 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix
 cd ~/
-wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && sudo tar -xvf tor-browser-linux-x86_64-14.0.4.tar.xz -C ~/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
+wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && tar -xvf tor-browser-linux-x86_64-14.0.4.tar.xz -C ~/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
 ~/tor-browser/Browser/start-tor-browser --display=:0
 echo "Tor and NoVNC has started Now go to https://localhost:5900 to access tor on the vnc server"
