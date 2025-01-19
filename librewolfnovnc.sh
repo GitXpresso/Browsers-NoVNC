bcb2f20c81cf706fd7d2a330c8d6114de8d887be
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 10:15:44 -0500

    Update librewolfnovnc.sh
    Removed a space and added "#!/bin/bash"

--- librewolfnovnc.sh
+++ librewolfnovnc.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 echo "updating system to successfully install required packages"
-
 sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix
