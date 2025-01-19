c8a001b1a2f120f4fa77ac99a87424e8c0b85dbf
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 16:52:31 -0500

    Update chromenovnc.sh
    Automatically removes .deb file

--- chromenovnc.sh
+++ chromenovnc.sh
@@ -4,8 +4,7 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
 cd ~/
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 google-chrome --display=:0
