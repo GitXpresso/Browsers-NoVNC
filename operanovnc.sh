bdb0c788308596dad307f2909d5435c3f0cf82e4
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 10:56:15 -0500

    Create operanovnc.sh

---
+++ operanovnc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Updating Your System"
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dialog
+cd ~/
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
+sudo apt update
+sudo apt install opera-stable
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
+opera-stable --display=:0
+echo "Opera started go to https://localhost:6080 to access NoVNC and Opera, and No it is not opera gx"
