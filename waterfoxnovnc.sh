7785f05039ef0d6597e8b19ffb97b5fd8c157a2c
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 16:47:25 -0500

    Removed useless command and echo commands

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -5,11 +5,9 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
 cd ~/
 wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib && sudo rm -rf waterfox-6.5.3.tar.bz2 
-sudo mv -f ~/waterfox.desktop /usr/share/applications
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 /usr/lib/waterfox/waterfox --display=:0
 echo "Finished, Now go to https://localhost:5900 to access waterfox the vnc server"
-echo "creating waterfox.desktop using the cat here document"
 
