cd057759c252261f80106c42c33a9048d6ac7c4b
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-19 16:45:01 -0500

    Removed unneeded echo command

--- zenbrowsernovnc.sh
+++ zenbrowsernovnc.sh
@@ -6,7 +6,6 @@ sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
 wget https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.bz2 && sudo tar -xvf zen.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf zen.linux-x86_64.tar.bz2
 git clone https://github.com/gitxpresso/linux-novnc.git
-echo "creating zen.desktop using cat here document"
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 /usr/lib/zen/zen --display=:0
