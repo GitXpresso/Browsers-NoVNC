e1de402acfb160b2e4e3011407570adb1b415b10
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-20 01:18:10 -0500

    Update tornovnc.sh
    Added "in-process-gpu"

--- tornovnc.sh
+++ tornovnc.sh
@@ -9,5 +9,5 @@ git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
-~/tor-browser/Browser/start-tor-browser --display=:0
+~/tor-browser/Browser/start-tor-browser --in-process-gpu --display=:0
 echo "Tor and NoVNC has started Now go to https://localhost:5900 to access tor on the vnc server"
