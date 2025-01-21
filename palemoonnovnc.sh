91dd2a78956f4d405ac12b18363dd8a142b59a0d
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-21 09:19:12 -0500

    Update palemoonnovnc.sh

--- palemoonnovnc.sh
+++ palemoonnovnc.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+echo "Updating Your System"
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils
+cd ~/
 wget https://filebin.net/6gfmzir57t8tohsh/palemoon-33.5.1.linux-x86_64-gtk2.tar.xz && tar -xvf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz -C ~/&& sudo rm -rf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz  
+git clone https://github.com/gitxpresso/linux-novnc.git
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
+~/ --display=:0
+echo "Pale Moon and NoVNC has started Now go to https://localhost:5900 to access tor on the vnc server"
