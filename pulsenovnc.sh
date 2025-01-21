e4f206c51099dd80db0d643dbb4bcf8ee94c9792
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-21 16:14:13 -0500

    Create pulsenovnc.sh

---
+++ pulsenovnc.sh
@@ -0,0 +1,1 @@
+wget https://github.com/pulse-browser/browser/releases/download/1.0.0-a.87/pulse-browser.linux.tar.bz2 && tar -xvf pulse-browser.linux.tar.bz2 && sudo rm -rf pulse-browser.linux.tar.bz2 
