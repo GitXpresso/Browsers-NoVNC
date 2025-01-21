afb5416c66faffae1c06f397ef6c83723766c48f
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-21 09:17:29 -0500

    Create palemoonnovnc.sh

---
+++ palemoonnovnc.sh
@@ -0,0 +1,1 @@
+wget https://filebin.net/6gfmzir57t8tohsh/palemoon-33.5.1.linux-x86_64-gtk2.tar.xz && tar -xvf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz -C ~/&& sudo rm -rf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz  
