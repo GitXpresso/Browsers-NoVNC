6d659d3350911e4d873841d9eb5d93a62c166c4b
GitXpresso <gitmocha@outlook.com>
2025-01-21 17:57:04 +0000

    j

--- seamonkeynovnc.sh
+++ seamonkeynovnc.sh
@@ -4,6 +4,22 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix
 wget https://archive.seamonkey-project.org/releases/2.53.20/linux-x86_64/en-US/seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 && sudo tar -xvf seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2
+cat << EOF > ~/seamonkey.desktop
+[Desktop Entry]
+Version=1.0
+StartupWMClass=seamonkey
+Icon=/usr/lib/seamonkey/browser/chrome/icons/default/default48.png
+Type=Application
+Categories=Network;WebBrowser;
+Exec=/usr/lib/seamonkey/seamonkey %u
+Name=Seamonkey
+Comment=Seamonkey Browser
+Terminal=false
+StartupNotify=true
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/qute;
+Keywords=Browser;
+EOF
+sudo mv ~/seamonkey.desktop /usr/share/applications/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 /usr/lib/seamonkey/seamonkey --display=:0
--- tornovnc.sh
+++ tornovnc.sh
@@ -5,6 +5,7 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils
 cd ~/
 wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && tar -xvf tor-browser-linux-x86_64-14.0.4.tar.xz -C ~/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
+sudo mv -f ~/tor-browser/start-tor-browser.desktop /usr/share/applications/
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
