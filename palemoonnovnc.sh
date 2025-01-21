1ece03e076f09af43167e61e06e931e75d27af27
GitXpresso <gitmocha@outlook.com>
2025-01-21 15:01:38 +0000

    clone repository to specific folder

--- palemoonnovnc.sh
+++ palemoonnovnc.sh
@@ -5,7 +5,7 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils
 cd ~/
 wget https://filebin.net/6gfmzir57t8tohsh/palemoon-33.5.1.linux-x86_64-gtk2.tar.xz && tar -xvf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz -C ~/&& sudo rm -rf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz  
-git clone https://github.com/gitxpresso/linux-novnc.git
+git clone https://github.com/gitxpresso/linux-novnc.git ~/linux-novnc
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
