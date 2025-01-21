c4b49507e9bd9be1ab2ff22dd77acf0523648849
GitXpresso <gitmocha@outlook.com>
2025-01-21 15:03:42 +0000

    added new package to required package  libdbus-glib-1-2:amd64

--- palemoonnovnc.sh
+++ palemoonnovnc.sh
@@ -2,7 +2,7 @@
 echo "Updating Your System"
 sudo apt update
 echo "Installing the required packages in order for the script to work properly"
-sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils libdbus-glib-1-2:amd64
 cd ~/
 wget https://filebin.net/6gfmzir57t8tohsh/palemoon-33.5.1.linux-x86_64-gtk2.tar.xz && tar -xvf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz -C ~/&& sudo rm -rf palemoon-33.5.1.linux-x86_64-gtk2.tar.xz  
 git clone https://github.com/gitxpresso/linux-novnc.git ~/linux-novnc
