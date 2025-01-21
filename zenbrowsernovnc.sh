27340dcb3fc8eb55f0e477e3ea6b19d1d764ee25
GitXpresso <gitmocha@outlook.com>
2025-01-21 17:49:01 +0000

    zen.desktop for openbox menu

--- zenbrowsernovnc.sh
+++ zenbrowsernovnc.sh
@@ -6,6 +6,22 @@ sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
 wget https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.bz2 && sudo tar -xvf zen.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf zen.linux-x86_64.tar.bz2
 git clone https://github.com/gitxpresso/linux-novnc.git
+cat << EOF > ~/zen.desktop
+[Desktop Entry]
+Version=1.0
+StartupWMClass=zen
+Icon=/usr/lib/zen/browser/chrome/icons/default/default48.png
+Type=Application
+Categories=Network;WebBrowser;
+Exec=/usr/lib/zen/zen %u
+Name=Pale Moon
+Comment=Zen Browser
+Terminal=false
+StartupNotify=true
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/qute;
+Keywords=Browser;
+EOF
+sudo mv ~/zen.desktop /usr/share/applications/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 /usr/lib/zen/zen --display=:0
