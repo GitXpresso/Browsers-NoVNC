8940448e28aa9f79ce5868727ac4c2d1322dd190
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-22 09:11:00 -0500

    Update torx11vnc.sh
    Adding tor desktop to open box menu automatically

--- torx11vnc.sh
+++ torx11vnc.sh
@@ -5,6 +5,7 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tar openbox kdialog zenity tilix apt-utils x11vnc
 cd ~/
 wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && tar -xvf tor-browser-linux-x86_64-14.0.4.tar.xz -C ~/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
+sudo mv -f ~/tor-browser/start-tor-browser.desktop /usr/share/applications/
 git clone https://github.com/gitxpresso/linux-novnc.git
 x11vnc -usepw -display :0 
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
