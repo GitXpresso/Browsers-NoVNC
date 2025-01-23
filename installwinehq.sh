176b001582d7d1fa1d2d9b70ffb821e2bd567d39
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-23 13:18:09 +0000

    added winehq installation shell script

---
+++ installwinehq.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
+sudo apt update
+sudo apt install -y --install-recommends winehq-stable
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+winecfg --display=:0
