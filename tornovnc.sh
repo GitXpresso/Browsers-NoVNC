0f0899c940321a054c7790933dc87ddc52fffed2
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-23 17:29:36 -0500

    Update tornovnc.sh

--- tornovnc.sh
+++ tornovnc.sh
@@ -5,7 +5,25 @@ echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils
 cd ~/
 wget https://www.torproject.org/dist/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz && tar -xvf tor-browser-linux-x86_64-14.0.4.tar.xz -C ~/ && sudo rm -rf tor-browser-linux-x86_64-14.0.4.tar.xz
-sudo mv -f ~/tor-browser/start-tor-browser.desktop /usr/share/applications/
+cd ~/
+wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib && sudo rm -rf waterfox-6.5.3.tar.bz2 
+git clone https://github.com/gitxpresso/linux-novnc.git
+cat << EOF > ~/torbrowser.desktop
+[Desktop Entry]
+Version=1.0
+StartupWMClass=torbrowser
+Icon=~/tor-browser/browser/chrome/icons/default/default48.png
+Type=Application
+Categories=Network;WebBrowser;
+Exec=~/tor-browser/Browser/start-tor-browser %u
+Name=Tor browser
+Comment=browse privately using the onion routing
+Terminal=false
+StartupNotify=true
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/qute;
+Keywords=Browser;
+EOF
+sudo mv -f ~/torbrowser.desktop /usr/share/applications/
 git clone https://github.com/gitxpresso/linux-novnc.git
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
