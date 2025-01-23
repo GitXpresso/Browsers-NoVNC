488d7e547f73eee197c9933424111c95d838461b
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-23 16:43:57 +0000

    updated

--- installwinehq.sh
+++ installwinehq.sh
@@ -5,9 +5,10 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 sudo apt update
-sudo apt install -y --install-recommends wine 
+sudo apt install -y --install-recommends wine-s 
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
 winecfg 
-wget https://github.com/GitXpresso/Browsers-NoVNC/releases/download/winehq/
+wget https://github.com/GitXpresso/Browsers-NoVNC/releases/download/winehq/OperaGXSetup.exe
 wine OperaGXSetup.exe --display=:0
