534d5fb7e4ada6463c47d60985021cca0a1280c8
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-24 15:46:04 -0500

    Update waterfoxnovnc.sh
    Remove waterfox.desktop contents and changed location of where wget grabs the file

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -4,24 +4,9 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
 cd ~/
-wget https://cdn1.waterfox.net/waterfox/releases/6.5.3/Linux_x86_64/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib && sudo rm -rf waterfox-6.5.3.tar.bz2 
+wget https://github.com/GitXpresso/Browsers-NoVNC/releases/download/waterfox/waterfox-6.5.3.tar.bz2 && sudo tar -xvf waterfox-6.5.3.tar.bz2 -C /usr/lib && sudo rm -rf waterfox-6.5.3.tar.bz2 
 git clone https://github.com/gitxpresso/linux-novnc.git
-cat << EOF > ~/waterfox.desktop
-[Desktop Entry]
-Version=1.0
-StartupWMClass=waterfox
-Icon=/usr/lib/waterfox/browser/chrome/icons/default/default48.png
-Type=Application
-Categories=Network;WebBrowser;
-Exec=/usr/lib/waterfox/waterfox %u
-Name=Waterfox
-Comment=Waterfox Web Browser
-Terminal=false
-StartupNotify=true
-MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/qute;
-Keywords=Browser;
-EOF
-sudo mv -f ~/waterfox.desktop /usr/share/applications/
+sudo mv -f ~/waterfox/waterfox.desktop /usr/share/applications/
 cat << EOF > ~/waternovnc
 #!/bin/sh -e
 ### BEGIN INIT INFO
