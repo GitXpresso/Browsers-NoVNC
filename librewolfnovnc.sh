0903111f727ceb1d86c70fd780279ce402284d13
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 11:31:39 -0500

    Update librewolfnovnc.sh

--- librewolfnovnc.sh
+++ librewolfnovnc.sh
@@ -9,5 +9,5 @@ sudo extrepo enable librewolf
 sudo apt update && sudo apt install librewolf -y
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-librewolf --display=:0
+librewolf --start-maximized --display=:0
 echo -e "librewolf has started go to https://localhost:6080 to access your vnc session"
