270595a6e3b0370a1a3a9289a6e1f95e37990e8f
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 13:43:59 -0500

    Create thoriumnovnc.sh

---
+++ thoriumnovnc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/Alex313031/thorium/releases/download/M128.0.6613.189/thorium-browser_128.0.6613.189_AVX.deb && sudo apt install -y ./thorium-browser_128.0.6613.189_AVX.deb && sudo rm -rf thorium-browser_128.0.6613.189_AVX.deb
