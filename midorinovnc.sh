652efe263b2c6d22415032e0a8f89e08d8bec32a
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 00:18:48 -0500

    Update and rename midorionovnc.sh to midorinovnc.sh

--- midorinovnc.sh
+++ midorinovnc.sh
@@ -6,7 +6,7 @@ sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox
 cd ~/
 wget https://filebin.net/7e896uopsecqi68f/midori_11.5_amd64.deb && sudo apt install ./midori_11.5_amd64.deb && sudo rm -rf midori_11.5_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
-websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080  sudo
 export DISPLAY=:0
 midori --display=:0
-echo "Opera started go to https://localhost:6080 to access NoVNC and Opera, and No it is not opera gx"
+echo "Midori started go to https://localhost:6080 to access NoVNC and Midori"
