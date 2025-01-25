f5245f1d30db050ce22ae4bc743e5940404195dd
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 13:25:25 -0500

    Update chromiumnovnc.sh

--- chromiumnovnc.sh
+++ chromiumnovnc.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
 sudo add-apt-repository ppa:savoury1/ffmpeg4
 sudo add-apt-repository ppa:savoury1/chromium
 sudo apt-get update
 sudo apt-get install chromium-browser 
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
 chromium-browser --in-process-gpu --no-sandbox --test-type --disable-dev-shm-usage --start-maximized --display=:0
+echo -e "chromium has started go to https://localhost:6080 to access your vnc session"
