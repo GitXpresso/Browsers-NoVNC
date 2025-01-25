d56941c341c92d13b657cf54ea0a90eb3580eb11
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 11:30:48 -0500

    Update icecatnovnc.sh

--- icecatnovnc.sh
+++ icecatnovnc.sh
@@ -25,5 +25,5 @@ sudo mv ~/icecat.desktop /usr/share/applications/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
-~/icecat/icecat --display=:0
-echo -e "librewolf has started go to https://localhost:6080 to access your vnc session"
+~/icecat/icecat --start-maximized --display=:0
+echo -e "icecat has started go to https://localhost:6080 to access your vnc session"
