70fe1e12f69787455c1404dc5c340af396e36d5a
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-25 11:32:58 -0500

    Update midorinovnc.sh

--- midorinovnc.sh
+++ midorinovnc.sh
@@ -4,9 +4,10 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dialog tilix dbus-x11
 cd ~/
+git clone https://github.com/gitxpresso/linux-novnc ~/linux-novnc
 wget https://filebin.net/7e896uopsecqi68f/midori_11.5_amd64.deb && sudo apt install ./midori_11.5_amd64.deb && sudo rm -rf midori_11.5_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 
 export DISPLAY=:0
 echo "Midori started go to https://localhost:6080 to access NoVNC and Midori"
-midori --display=:0
+midori --start-maximized --display=:0
