13ea9b25c6f2998b6ea921574942223cdff66894
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-28 17:26:02 +0000

    added -y to make the script automatic

--- playonlinuxnovnc.sh
+++ playonlinuxnovnc.sh
@@ -5,8 +5,8 @@ sudo apt install -y curl wget novnc websockify tigervnc-standalone-server tar op
 wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
 sudo wget http://deb.playonlinux.com/playonlinux_cosmic.list -O /etc/apt/sources.list.d/playonlinux.list
 sudo apt-get update
-sudo apt-get install playonlinux
+sudo apt-get install playonlinux -y
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-export DISPLAY=:0
+export DISPLAY=:
 playonlinux 
