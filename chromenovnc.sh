641bbf40535ce4126598b8c3796dbf8ed2a9d568
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-29 18:20:04 +0000

    updated

--- chromenovnc.sh
+++ chromenovnc.sh
@@ -2,7 +2,7 @@
 echo "Updating Your System"
 sudo apt update
 echo "Installing the required packages in order for the script to work properly"
-sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix dbus-x11
 cd ~/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
