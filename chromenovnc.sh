cec059a82edab99243dfd9f509e0ad7e0e5dbaf0
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-29 18:15:19 +0000

    updated

--- chromenovnc.sh
+++ chromenovnc.sh
@@ -7,5 +7,5 @@ cd ~/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install -y ./google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-google-chrome --no-sandbox --start-maximized --display=:0
+google-chrome --kiosk --display=:0
 echo -e "chrome has started go to https://localhost:6080 to access your vnc session"
