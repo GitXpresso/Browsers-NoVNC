6e74c5335eca201246ab6653810fdf6e94bf0868
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-30 17:01:17 +0000

    updated

--- seamonkeynovnc.sh
+++ seamonkeynovnc.sh
@@ -2,7 +2,7 @@
 echo "updating system to successfully install required packages"
 sudo apt update
 echo "Installing required packages"
-sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix libdbus-glib-1-2:amd64 libgtk2.0-0
 wget https://archive.seamonkey-project.org/releases/2.53.20/linux-x86_64/en-US/seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 && sudo tar -xvf seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf seamonkey-2.53.20.en-US.linux-x86_64.tar.bz2
 cat << EOF > ~/seamonkey.desktop
 [Desktop Entry]
@@ -22,5 +22,7 @@ EOF
 sudo mv ~/seamonkey.desktop /usr/share/applications/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-/usr/lib/seamonkey/seamonkey --start-maximized --display=:0
-echo -e "librewolf has started go to https://localhost:6080 to access your vnc session"
+echo -e "seamonkey has started go to https://localhost:6080 to access your vnc session"
+sudo ln -s /usr/lib/seamonkey/seamonkey /usr/bin/startseamonkey
+export DISPLAY=:0
+startseamonkey
