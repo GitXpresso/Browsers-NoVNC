2f31a3b656efd7e2de9bf7ebb6567c49d32ec16e
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-30 16:56:19 +0000

    updated

--- tornovnc.sh
+++ tornovnc.sh
@@ -24,6 +24,7 @@ sudo mv -f ~/torbrowser.desktop /usr/share/applications/
 git clone https://github.com/gitxpresso/linux-novnc.git ~/linux-novnc
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-export DISPLAY=:0
 echo "Tor and NoVNC has started Now go to https://localhost:5900 to access tor on the vnc server"
-~/tor-browser/Browser/start-tor-browser --start-maximized --display=:0
+sudo ln -s ~/tor-browser/Browser/start-tor-browser /usr/bin/starttor
+export DISPLAY=:0
+starttor
