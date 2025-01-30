3e36a15f667c958aef75eb95863cd54db1e1273a
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-30 17:10:03 +0000

    updated

--- floorpnovnc.sh
+++ floorpnovnc.sh
@@ -4,7 +4,7 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 dialog tilix
 cd ~/
-wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2 -C /usr/lib && sudo rm -rf floorp-11.22.0.linux-x86_64.tar.bz2 
+wget https://github.com/Floorp-Projects/Floorp/releases/latest/download/floorp-11.22.0.linux-x86_64.tar.bz2 && sudo tar -xvf floorp-11.22.0.linux-x86_64.tar.bz2 -C ~/ && sudo rm -rf floorp-11.22.0.linux-x86_64.tar.bz2 
 cat << EOF > ~/floorp.desktop
 [Desktop Entry]
 Version=1.0
@@ -23,6 +23,7 @@ EOF
 sudo mv ~/floorp.desktop /usr/share/applications/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
-export DISPLAY=:0
-/usr/lib/floorp/floorp --start-maximized --display=:0
 echo "floorp has started go to https://localhost:5900 to access floorp in the vnc session"
+export DISPLAY=:0
+sudo ln -s ~/floorp/floorp /usr/bin/startfloorp
+startfloorp
