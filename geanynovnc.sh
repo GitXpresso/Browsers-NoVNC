cc126456884f1a597cf60851d3c3e4dab4def0b1
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-31 18:25:00 +0000

    added geany

---
+++ geanynovnc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Updating Your System"
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils geany
+cd ~/
+git clone https://github.com/gitxpresso/linux-novnc ~/linux-novnc
+tigervncseruver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
+geany
