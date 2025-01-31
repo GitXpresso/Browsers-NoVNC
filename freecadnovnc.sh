2c12555bf869a84b3c029eb700da05376e67ed1d
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-31 15:22:13 +0000

    updated

--- freecadnovnc.sh
+++ freecadnovnc.sh
@@ -4,6 +4,11 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox kdialog zenity tilix apt-utils freecad
 cd ~/
+sudo apt-get install pyqt5-dev
+sudo apt-get install pyqt5-dev-tools
+cd /usr/bin/
+ln -s pyrcc5 pyside2-rcc
+ln -s pyuic5 pyside2-uic
 git clone https://github.com/gitxpresso/linux-novnc ~/linux-novnc
 tigervncseruver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
