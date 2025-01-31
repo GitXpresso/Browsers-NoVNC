dbbebed04df7613679ee7fc65d52c521dcb666d4
Kanashi Tamashi <gitmocha@outlook.com>
2025-01-31 13:30:03 +0000

    updated

--- firefoxnovnc.sh
+++ firefoxnovnc.sh
@@ -19,6 +19,7 @@ Pin-Priority: 1000
 echo "updating and installing firefox"
 sudo apt-get update && sudo apt-get install firefox -y
 echo "firefox installed, now starting the novnc server"
+curl -fsSL https://raw.githubusercontent.com/GitXpresso/Browsers-NoVNC/refs/heads/main/firefoxdesktop | bash
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPlAY=:0
