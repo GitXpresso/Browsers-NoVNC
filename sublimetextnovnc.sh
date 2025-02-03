c07e88bb6d99965c2fcafc5b5b275588b237413d
William . <gitmocha@outlook.com>
2025-02-03 14:20:56 +0000

    updated

--- sublimetextnovnc.sh
+++ sublimetextnovnc.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+echo "Updating Your System"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt update
+echo "Installing the required packages in order for the script to work properly"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix dbus-x11 sublime-text
