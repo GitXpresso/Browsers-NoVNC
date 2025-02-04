6096f3b895694e8c0dc4c8c8e64557ff5d76b401
William . <gitmocha@outlook.com>
2025-02-04 15:31:17 +0000

    updated

--- siagnovnc.sh
+++ siagnovnc.sh
@@ -4,6 +4,7 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix 
 wget https://siag.nu/pub/siag/siag-3.6.1.tar.gz && tar -xvf siag-3.6.1.tar.gz && sudo rm -rf siag-3.6.1.tar.gz
+cd ~/siag-3.6.1 && ./configure && make && make install
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
