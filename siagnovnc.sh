3a13bb3871cd475cafe70b243f092bbc8f8fa18c
William . <gitmocha@outlook.com>
2025-02-04 15:36:17 +0000

    u

--- siagnovnc.sh
+++ siagnovnc.sh
@@ -4,6 +4,9 @@ sudo apt update
 echo "Installing required packages"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 tilix 
 wget https://siag.nu/pub/siag/siag-3.6.1.tar.gz && tar -xvf siag-3.6.1.tar.gz && sudo rm -rf siag-3.6.1.tar.gz
+wget -q -O - https://raw.githubusercontent.com/xpack/assets/master/scripts/install-nvm-node-npm-xpm.sh | ${SHELL}
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
 cd ~/siag-3.6.1 && ./configure && make && make install
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
