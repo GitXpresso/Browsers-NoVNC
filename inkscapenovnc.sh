6a9f2646a28d5c387dddc35fea12f1f3ecce25ff
William . <gitmocha@outlook.com>
2025-02-06 18:23:44 +0000

    updated

--- inkscapenovnc.sh
+++ inkscapenovnc.sh
@@ -2,7 +2,7 @@
 echo "updating system to successfully install required packages"
 sudo apt update
 echo "Installing required packages"
-sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 pixmap inkscape tilix gtk2-engines-pixbuf
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 pixmap inkscape tilix gtk2-engines-pixbuf dialog  yaru-theme-icon
 cd ~/
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
