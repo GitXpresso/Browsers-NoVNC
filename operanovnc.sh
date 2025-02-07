efbce6ea58c57669a57d46e29010a163205c7a26
William . <gitmocha@outlook.com>
2025-02-07 17:46:21 +0000

    updated

--- operanovnc.sh
+++ operanovnc.sh
@@ -4,7 +4,7 @@ sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dialog tilix dbus-x11
 cd ~/
-wget https://filebin.net/f6atxfnca1z89hua/opera-stable_116.0.5366.35_amd64.deb && sudo apt install -y ./opera-stable_116.0.5366.35_amd64.deb && sudo rm -rf opera-stable_116.0.5366.35_amd64.deb 
+wget https://github.com/GitXpresso/Browsers-NoVNC/releases/download/TarAndDeb/opera-stable_116.0.5366.35_amd64.deb && sudo apt install -y ./opera-stable_116.0.5366.35_amd64.deb && sudo rm -rf opera-stable_116.0.5366.35_amd64.deb 
 tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
 websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
 export DISPLAY=:0
