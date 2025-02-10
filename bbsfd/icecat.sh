1e9b5d7d06b6bdf5649666d9861c1203069b48f5
William <126926699+GitXpresso@users.noreply.github.com>
2025-02-10 13:22:05 +0000

    updated

--- bbsfd/icecat.sh
+++ bbsfd/icecat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install -y wget tar dbus-x11
-wget https://icecatbrowser.org/assets/icecat/115.18.0/icecat-115.18.0.en-US.linux-x86_64.tar.bz2 && sudo tar -xvf icecat-115.18.0.en-US.linux-x86_64.tar.bz2 -C ~/ && sudo rm -rf icecat-115.18.0.en-US.linux-x86_64.tar.bz2 
-sudo ln -s ~/icecat/icecat /usr/bin/icecat
+wget https://icecatbrowser.org/assets/icecat/115.18.0/icecat-115.18.0.en-US.linux-x86_64.tar.bz2 && sudo tar -xvf icecat-115.18.0.en-US.linux-x86_64.tar.bz2 -C /opt && sudo rm -rf icecat-115.18.0.en-US.linux-x86_64.tar.bz2 
+sudo ln -s /opt/icecat/icecat /usr/bin/icecat
