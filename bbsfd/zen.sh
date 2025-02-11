85eb1fdaf9bde1b525aa516101199f5d3dbf4a46
William <126926699+GitXpresso@users.noreply.github.com>
2025-02-11 13:35:43 +0000

    Create zen.sh

---
+++ bbsfd/zen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/zen-browser/desktop/releases/download/1.7.6b/zen-x86_64.AppImage && sudo chmod u+x ./zen-x86_64.AppImage && ./zen-x86_64.AppImage --appimage-extract && ~/squashfs-root/zen sudo ln -s ~/squashfs-root/zen /usr/bin/zen
+sudo mv -f ~/squashfs-root/usr/share/icons/hicolor/128x128/apps/zen.png /usr/share/icons/hicolor/128x128/apps/zen.png
