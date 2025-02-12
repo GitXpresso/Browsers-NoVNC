d39398c5c2a35e08d84ab697945ac6c134d19168
William <126926699+GitXpresso@users.noreply.github.com>
2025-02-12 12:57:50 +0000

    Create audacitynovnc.sh

---
+++ audacitynovnc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "updating system to successfully install required packages"
+sudo apt update
+echo "Installing required packages"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox dbus-x11 audacity dialog tilix 
+cd ~/
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:5900
+export DISPLAY=:0
+echo -e "audacity  has started go to https://localhost:6080 to access your vnc session"
+audacity 
