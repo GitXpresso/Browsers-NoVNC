f5363ed0e6d815ae8f62ac6a895dad3d1234a007
William . <gitmocha@outlook.com>
2025-02-13 17:12:15 +0000

    updated

--- Waterfox/ghcrbuild.sh
+++
@@ -1,2 +0,0 @@
-docker build -t ghcr.io/gitxpresso/IMAGE_NAME:latest .
-docker push ghcr.io/gitxpresso/IMAGENAME:latest
---
+++ Zen/ghcrbuild.sh
@@ -0,0 +1,2 @@
+docker build -t ghcr.io/gitxpresso/zenkasmvnc:latest .
+docker push ghcr.io/gitxpresso/IMAGENAME:latest
