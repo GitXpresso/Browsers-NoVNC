db98f8473043521dcbbe5e38bff6745bb3acc4cb
William . <gitmocha@outlook.com>
2025-02-17 19:30:24 +0000

    updated

---
+++ debcreatingsh/installfloorp.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt install curl sudo gpg -y
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+sudo apt update
+sudo apt install floorp -y
