a9ec00903956921fde6f192320076bfd493b6d8c
William . <gitmocha@outlook.com>
2025-02-18 14:27:30 +0000

    user input testing

--- debcreatingsh/tar2deb.sh
+++ debcreatingsh/tar2deb.sh
@@ -4,15 +4,19 @@ sudo apt-get install wget build-essential devscripts debhelper busybox -y
 # Edit the Export Variables in order for this file to work successfully
 export TAR_URL="https://github.com/zen-browser/desktop/releases/download/1.7.6b/zen.linux-x86_64.tar.xz"
 export TAR_DIR="zen"
-export DEB_DIR="zen-1.7.6b"
 export NAME_OF_IMAGE="zen"
 export TAR_EXEC="zen"
 export TAR_EXEC2="zen-bin"
-wget -P ~/ ${TAR_URL} && tar -xvf ~/*.tar.xz -C ~/ && sudo rm -f ~/*.tar.xz
-mkdir ~/${DEB_DIR}
-mkdir -p ~/${DEB_DIR}/DEBIAN
+echo -n "Please enter the url of the tar file:"
+read TAR_URL
+wget -P ~/ $TAR_URL && tar -xvf ~/*.tar.xz -C ~/ && sudo rm -f ~/*.tar.xz
+echo -n "Please enter your base directory name: "
+read DEB_DIR
+
+mkdir ~/$DEB_DIR
+mkdir -p ~/$DEB_DIR/DEBIAN
 # You are going to have to edit the following contents below 
-cat << EOF >~/${DEB_DIR}/DEBIAN/control
+cat << EOF >~/$DEB_DIR/DEBIAN/control
 Package: zen
 Version: 1.7.6b
 Section: base
@@ -22,14 +26,14 @@ Maintainer: William G
 Description: Zen browser .deb file
 
 EOF
-mkdir -p ~/${DEB_DIR}/usr/bin
-mkdir -p ~/${DEB_DIR}/usr/lib/zen
-mkdir -p ~/${DEB_DIR}/usr/lib
-mkdir -p ~/${DEB_DIR}/usr/share/applications/
-mkdir -p ~/${DEB_DIR}/usr/share/icons/hicolor/48x48/apps/
+mkdir -p ~/$DEB_DIR/usr/bin
+mkdir -p ~/$DEB_DIR/usr/lib/zen
+mkdir -p ~/$DEB_DIR/usr/lib
+mkdir -p ~/$DEB_DIR/usr/share/applications/
+mkdir -p ~/$DEB_DIR/usr/share/icons/hicolor/48x48/apps/
 echo -e "copying executable files to zen-1.7.6b"
 echo "copying zen.png to zen-1.7.6b"
-cat << EOF >~/${DEB_DIR}/usr/share/applications/zen.desktop
+cat << EOF >~/$DEB_DIR/usr/share/applications/zen.desktop
 [Desktop Entry]
 Version=1.0
 # Edit "zen" to the executable file from the tar
@@ -47,9 +51,9 @@ MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rd
 Keywords=Browser;
 EOF
 echo "copying zen.png to zen-1.7.6b"
-cp -r ~/${TAR_DIR}/* ~/${DEB_DIR}/usr/lib/zen/
-cp -r ~/${TAR_DIR}/lib*.so ~/${DEB_DIR}/usr/lib/
-cp -r ~/${TAR_DIR}/browser/chrome/icons/default/default48.png ~/${DEB_DIR}/usr/share/icons/hicolor/48x48/apps/${NAME_OF_IMAGE}.png
+cp -r ~/${TAR_DIR}/* ~/$DEB_DIR/usr/lib/zen/
+cp -r ~/${TAR_DIR}/lib*.so ~/$DEB_DIR/usr/lib/
+cp -r ~/${TAR_DIR}/browser/chrome/icons/default/default48.png ~/$DEB_DIR/usr/share/icons/hicolor/48x48/apps/${NAME_OF_IMAGE}.png
 busybox ln -s ~/zen-1.7.6b/usr/lib/zen/${TAR_EXEC}  ~/zen-1.7.6b/usr/bin 
 busybox ln -s ~/zen-1.7.6b/usr/lib/zen/${TAR_EXEC2}  ~/zen-1.7.6b/usr/bin
-dpkg-deb --build ~/${DEB_DIR}
+dpkg-deb --build ~/$DEB_DIR
