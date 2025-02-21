5e939abb70c55445f7735b8810e82a5bddf114eb
William . <gitmocha@outlook.com>
2025-02-21 13:36:51 +0000

    updated, testing

--- tar2debversionprompt.sh
+++ tar2debversionprompt.sh
@@ -1,28 +1,17 @@
-clear
+#!/bin/bash
+
+# Function to check if input is a number
+is_number() {
+    [[ $1 =~ ^[0-9]+$ ]]
+}
+
+# Prompt user for input
 while true; do
-        read -p "enter the version of your package: " Version
-        if [[ $Version =~ ^[0-9]+$ ]]; then
-            clear
-            echo "The version of your package you've entered: $Version"
-        if [[ $answer =~ ^[0-9]+$ ]]; then
-            read -p "do you want to change version of your package? (yes/no/y/n)" answer
-        if [ y = "$answer" ]; then
-            echo "changing version of package"
-            clear
-            bash ./test2.sh 
-        elif [ n = "$answer" ]; then
-            echo "not changing package version: $Version"
-            break
-        if 
-            [ yes = "$answer" ]; then
-            echo "changing version of package"
-            clear
-            bash ./test2.sh
-        elif [ no = "$answer" ]; then
-            echo "not changing package version: $Version"
-            break       
-        else
-            echo "Invalid input. Not a Number, Try Again"
-        fi
+    read -p "Please enter a number: " user_input
+    if is_number "$user_input"; then
+        echo "Thank you! You entered the number: $user_input"
+        break
+    else
+        echo "Invalid input. Please enter a valid number."
     fi
-done      
+done
