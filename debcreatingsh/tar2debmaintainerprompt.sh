651be4a23bbc54651e7074a60854d995a017371f
William <126926699+GitXpresso@users.noreply.github.com>
2025-02-24 13:16:46 +0000

    Update tar2debmaintainerprompt.sh

--- debcreatingsh/tar2debmaintainerprompt.sh
+++ debcreatingsh/tar2debmaintainerprompt.sh
@@ -1,15 +1,42 @@
-# Function to check if input is a number
-is_letter() {
-    [[ "$1" =~ ^[Aa-Zz ]+$ ]];
+#!/bin/bash
+
+# Function to validate and create full name
+create_full_name() {
+  local input=("$@")
+  local first_name=""
+  local last_name=""
+  local valid_input=true
+
+  # Validate and separate first and last name
+  for word in "${input[@]}"; do
+    if [[ ! "$word" =~ [0-9] ]]; then
+      if [ -z "$first_name" ]; then
+        first_name="$word"
+      elif [ -z "$last_name" ]; then
+        last_name="$word"
+      fi
+    else
+      valid_input=false
+      break
+    fi
+  done
+
+  if [ "$valid_input" = true ] && [ -n "$first_name" ] && [ -n "$last_name" ]; then
+    full_name="$first_name $last_name"
+    echo "Valid full name: $full_name"
+    export Maintainer="$full_name"
+  else
+    echo "Invalid input. Please enter only letters and spaces for the first and last name."
+  fi
 }
 
 # Prompt user for input
 while true; do
-    read -p "Please enter your name or some elses name as the maintainer for the package: " Maintainer
-    if is_letter "$Maintainer"; then
+    read -p "Please enter your name or some elses name as the maintainer for the package: " -a Maintainer
+    if IFS=" " create_full_name "${Maintainer[@]}"; then
         clear
         echo "The Maintainer of your package is: $Maintainer"
-        read -p "do you want to change the version of your package? (yes/no)" yesorno
+        read -p "do you want to change the maintainer name of your package? (yes/no)" yesorno
     if [ "$yesorno" = yes ]; then
     clear
     bash ./tar2debmaintainerprompt.sh
