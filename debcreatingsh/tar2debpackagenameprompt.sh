26939d84d096949aac2d493069d45ad4dda97fc7
William <126926699+GitXpresso@users.noreply.github.com>
2025-02-24 13:41:08 +0000

    Update tar2debpackagenameprompt.sh

--- debcreatingsh/tar2debpackagenameprompt.sh
+++ debcreatingsh/tar2debpackagenameprompt.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Function to check if input is a number
 is_letter() {
     [[ "$1" =~ ^[Aa-zZ]+$ ]];
@@ -11,7 +9,7 @@ while true; do
     if is_letter "$PackageName"; then
         clear
         echo "The name of your package is: $PackageName"
-        read -p "do you want to change the name of your package? (yes/no)" yesorno
+        read -p "do you want to change the name of your package? (yes/no) " yesorno
     if [ "$yesorno" = yes ]; then
     clear
     bash ./tar2debpackagenameprompt.sh
@@ -29,33 +27,3 @@ while true; do
         echo "Invalid input. Please enter a valid letter."
     fi
 done
-is_number() {
-    [[ "$1" =~ ^[0-9+\.]+$ ]];
-}
-
-# Prompt user for input
-while true; do
-    read -p "Please enter a Version for your package: " Version
-    if is_number "$Version"; then
-        clear
-        echo "The version of your package is: $Version"
-        read -p "do you want to change the version of your package? (yes/no)" yesorno
-    if [ "$yesorno" = yes ]; then
-    clear
-    bash ./tar2debversionprompt.sh
-    elif [ "$yesorno" = no ]; then
-    clear
-    echo "not changing package version"
-    clear
-    break
-    else
-    echo "Not a number."
-    exit 1
-    fi
-        break
-    else
-        echo "Invalid input. Please enter a valid number."
-    fi
-done
-debdir="$PackageName-$Version"
-export DEB_DIR="$debdir"
