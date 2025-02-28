63aa98f7ebea4adb65a9623590b165adebb7f27b
Kanashi Tamashi <gitmocha@outlook.com>
2025-02-28 13:45:23 +0000

    updated

--- debcreatingsh/updatedzen.sh
+++ debcreatingsh/updatedzen.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-
+ls48x48() {
 # Directory to check
 directory="$HOME/usr/share/icons/hicolor/48x48/apps"
 
 
 # Count the number of files in the directory
 file_count=$(ls -1 "$directory" | wc -l)
-echo "Number of files in the directory: $file_count"
 
 # Get the filenames and export as a variable
 file_list=$(ls -1 "$directory")
-echo "Files in the directory: $file_list"
 
 # Create a numeric list file with filenames next to it
 numeric_list_file="$directory/numeric_list.txt"
@@ -20,3 +17,5 @@ EOL
 numberic48x48=$(cat -v $numeric_list_file)
 # Display the numeric list file path
 echo "$numberic48x48"
+}
+ls48x48
