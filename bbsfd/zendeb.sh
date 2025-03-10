9616c1a8bbeb93143637eaad20ba0d1b874c0dd8
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-10 16:28:20 +0000

    Update zendeb.sh

--- bbsfd/zendeb.sh
+++ bbsfd/zendeb.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # Install Zen Browser to create .deb package
-wget https://github.com/zen-browser/desktop/releases/download/1.7.6b/zen.linux-x86_64.tar.xz && tar -xvf zen.linux-x86_64.tar.xz -C ~/
-mkdir -p ~/zen-1.7.6b
-mkdir -p ~/zen-1.7.6b/DEBIAN
-cat << EOF >~/zen-1.7.6b/DEBIAN/control
+wget https://github.com/zen-browser/desktop/releases/download/1.9b/zen.linux-x86_64.tar.xz && tar -xvf zen.linux-x86_64.tar.xz -C ~/
+mkdir -p ~/zen-1.9b
+mkdir -p ~/zen-1.9b/DEBIAN
+cat << EOF >~/zen-1.9b/DEBIAN/control
 Package: zen
-Version: 1.7.6b
+Version: 1.9b
 Section: base
 Priority: optional
 Architecture: all
 Maintainer: William G
 Description: Zen browser .deb file
 EOF
-mkdir -p ~/zen-1.7.6b/usr/bin
-mkdir -p ~/zen-1.7.6b/usr/lib/zen
-mkdir -p ~/zen-1.7.6b/usr/lib
-mkdir -p ~/zen-1.7.6b/usr/share/applications
-mkdir -p ~/zen-1.7.6b/usr/share/icons/hicolor/48x48/apps
+mkdir -p ~/zen-1.9b/usr/bin
+mkdir -p ~/zen-1.9b/usr/lib/zen
+mkdir -p ~/zen-1.9b/usr/lib
+mkdir -p ~/zen-1.9b/usr/share/applications
+mkdir -p ~/zen-1.9b/usr/share/icons/hicolor/48x48/apps
 echo -e "copying executable files to zen-1.7.6b"
-cp -r ~/zen/zen ~/zen-1.7.6b/usr/bin/
-cp -r ~/zen/zen-bin ~/zen-1.7.6b/usr/bin/
+cp -r ~/zen/zen ~/zen-1.9b/usr/bin/
+cp -r ~/zen/zen-bin ~/zen-1.9b/usr/bin/
 echo "copying zen.png to zen-1.7.6b"
-cat << EOF >~/zen-1.7.6b/usr/share/applications/zen.desktop
+cat << EOF >~/zen-1.9b/usr/share/applications/zen.desktop
 [Desktop Entry]
 Version=1.0
 StartupWMClass=zen
@@ -37,7 +37,6 @@ MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rd
 Keywords=Browser;
 EOF
 echo "copying zen.png"
-cp -r ~/zen/ ~/zen-1.7.6b/usr/lib/zen
-cp -r ~/zen/lib*.so ~/zen-1.7.6b/usr/lib
-cp -r ~/zen/browser/chrome/icons/default/default48.png ~/zen-1.7.6b/usr/share/icons/hicolor/48x48/apps/zen.png
-dpkg-deb --build ~/zen-1.7.6b
+cp -r ~/zen/lib*.so ~/zen-1.9b/usr/lib
+cp -r ~/zen/browser/chrome/icons/default/default48.png ~/zen-1.9b/usr/share/icons/hicolor/48x48/apps/zen.png
+dpkg-deb --build ~/zen-1.9b
