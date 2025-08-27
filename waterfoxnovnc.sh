cac38ad2dab5ee8c2dfe5e33fcfaea4b4d205b48
William <126926699+GitXpresso@users.noreply.github.com>
2025-08-27 12:06:16 +0000

    Update waterfoxnovnc.sh
    Changes 1

--- waterfoxnovnc.sh
+++ waterfoxnovnc.sh
@@ -3,25 +3,23 @@ echo "Updating Your System"
 sudo apt update
 echo "Installing the required packages in order for the script to work properly"
 
-pkgs=[ "wget" "novnc" "websockify" "tigervnc-standalone-server" "dbus-x11" "tar" "openbox" "tilix" "libasound-dev" ]
+pkgs=( "wget" "novnc" "websockify" "tigervnc-standalone-server" "dbus-x11" "tar" "openbox" "tilix" "libasound-dev" )g
+for pkgs1 in ${pkgs[@]}; do
+  dpkg -l | grep -qw $pkgs1
+
+  if [ $? -eq 0 ]; then
+    echo "$pkgs1 already installed"
+  else
+    echo "$pkgs1 not installing, installing $pkgs1..."
+    sudo apt install -y $pkgs1
+  fi
+  sleep 0.2
+  clear
+done
 cd ~/ 
 wget https://cdn1.waterfox.net/waterfox/releases/6.6.1/Linux_x86_64/waterfox-6.6.1.tar.bz2 && sudo tar -xvf ~/waterfox-6.6.1.tar.bz2 -C /usr/lib && sudo rm -rf waterfox-6.6.1.tar.bz2 
 git clone https://github.com/gitxpresso/linux-novnc ~/linux-novnc
-cat << EOF > ~/waterfox.desktop
-[Desktop Entry]
-Version=6.5.5
-StartupWMClass=waterfox
-Icon=/usr/lib/waterfox/browser/chrome/icons/default/default48.png
-Type=Application
-Categories=Network;WebBrowser;
-Exec=/usr/lib/waterfox/waterfox %u
-Name=Waterfox
-Comment=Waterfox Web Browser
-Terminal=false
-StartupNotify=true
-MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/qute;
-Keywords=Browser;
-EOF
+
 sudo mv -f ~/waterfox.desktop /usr/share/applications/
 clear
 read -p "do you want to add a password to the novnc server? (yes/no) " yesorno
